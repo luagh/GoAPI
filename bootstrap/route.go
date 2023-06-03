6429e72328f27b9e7ff024fea1213626c367d29c
laugh <laugh@email.com>
2023-06-03 13:26:35 +0800

    feat: 新增API路由

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,1 +1,30 @@
 package bootstrap
+
+import "github.com/gin-gonic/gin"
+
+//  路由初始化
+
+func SetupRoute(router *gin.Engine) {
+	// 注册全局中间件
+	registerGlobalMiddleWare(router)
+	//  注册 API 路由
+	router.RegisterAPIRoutes(router)
+	//  配置 404 路由
+	setup404Handler(router)
+}
+
+// 配置 404 路由
+func setup404Handler(router *gin.Engine) {
+ router.NoRoute(func(c *gin.Context) {
+
+	 acceptString :=c.Request.Header.Get("Accept")
+	 if strings.c
+ })
+}
+
+// 注册全局中间件
+func registerGlobalMiddleWare(router *gin.Engine) {
+	router.Use(
+		gin.Logger(),
+		gin.Recovery())
+}
