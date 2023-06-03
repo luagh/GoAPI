4df85142625fc5ca14fa2625aff8981a6f0905b6
laugh <laugh@email.com>
2023-06-03 14:47:44 +0800

    feat:路由判断

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,6 +1,10 @@
 package bootstrap
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
 
 //  路由初始化
 
@@ -15,11 +19,18 @@ func SetupRoute(router *gin.Engine) {
 
 // 配置 404 路由
 func setup404Handler(router *gin.Engine) {
- router.NoRoute(func(c *gin.Context) {
+	router.NoRoute(func(c *gin.Context) {
 
-	 acceptString :=c.Request.Header.Get("Accept")
-	 if strings.c
- })
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
 }
 
 // 注册全局中间件
