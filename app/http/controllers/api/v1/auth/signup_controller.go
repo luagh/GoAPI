8ed0d7c6327f2c896b3f2c072390650751c6c38b
laugh <laugh@email.com>
2023-06-04 20:09:11 +0800

    feat:新增邮箱是否被注册功能的实现

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -18,7 +18,7 @@ type SingupController struct {
 
 func (sc *SingupController) IsPhoneExist(c *gin.Context) {
 
-	//请求的对象
+	//初始化请求的对象
 
 	request := requests.SignupPhoneExistRequest{}
 
@@ -29,9 +29,11 @@ func (sc *SingupController) IsPhoneExist(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
 			"error": err.Error(),
 		})
+		// 打印错误信息 并返回
 		fmt.Println(err.Error())
 		return
 	}
+	// 表单验证
 	errs := requests.ValidateSignupPhoneExist(&request, c)
 	if len(errs) > 0 {
 		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
@@ -44,4 +46,35 @@ func (sc *SingupController) IsPhoneExist(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
+}
+
+// 检测邮箱是否被注册
+func (sc *SingupController) IsEmailExist(c *gin.Context) {
+	request := requests.SignupEmailExistRequest{}
+	//解析json请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+
+		//解析失败 返回一个状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+		// 打印错误信息 并返回
+		fmt.Println(err.Error())
+		return
+	}
+	// 表单验证
+
+	err := requests.ValidateSignupEmailExist(&request, c)
+
+	if len(err) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err,
+		})
+		return
+	}
+	//检查数据库返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsEmailExist(request.Email),
+	})
+
 }
