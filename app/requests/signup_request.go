3f3d7e6ef8096c5cc0187145be6d1e916561716b
laugh <laugh@email.com>
2023-06-04 20:08:17 +0800

    feat:新增邮箱验证功能

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -6,6 +6,7 @@ import (
 )
 
 // 创建验证器
+// 手机验证器
 type SignupPhoneExistRequest struct {
 	Phone string `json:"phone,omitempty" valid:"phone"`
 }
@@ -28,6 +29,35 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 		Rules:         rules,
 		Messages:      message,
 		TagIdentifier: "valid", // 模型中的SignupPhoneExistRequest Struct 标签标识符
+	}
+	return govalidator.New(opts).ValidateStruct()
+}
+
+// email验证器
+type SignupEmailExistRequest struct {
+	Email string `json:"email,omitempty" valid:"email"`
+}
+
+func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
+	//自定义验证规则
+	rules := govalidator.MapData{
+		"email": []string{"required", "min:4", "max:30", "email"},
+	}
+	// 自定义验证出错时的提示
+	message := govalidator.MapData{
+		"email": []string{
+			"required:Email 为必填项",
+			"min:Email 长度需大于 4",
+			"max:Email 长度需小于 30",
+			"email:Email 格式不正确，请提供有效的邮箱地址",
+		},
+	}
+	//配置初始化
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		Messages:      message,
+		TagIdentifier: "valid", // 模型中的 SignupEmailExistRequest Struct 标签标识符
 	}
 	return govalidator.New(opts).ValidateStruct()
 }
