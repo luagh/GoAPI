8db7c47a8d50a32a000cd701ec4f931779438e1b
laugh <laugh@email.com>
2023-06-04 17:39:18 +0800

    注册路由判断手机号是否注册

--- routes/api.go
+++ routes/api.go
@@ -2,18 +2,19 @@ package routes
 
 //注册网页相关路由
 import (
+	"GOHUB/app/http/controllers/api/v1/auth"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func RegisterAPIRoutes(r *gin.Engine) {
-
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"hello": "wdf",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SingupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
