aeb0b1b7cdeb023b305fb1a936f953a296a4dc74
laugh <laugh@email.com>
2023-06-04 20:10:17 +0800

    feat:注册路由来判断邮箱

--- routes/api.go
+++ routes/api.go
@@ -15,6 +15,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			suc := new(auth.SingupController)
 			// 判断手机是否已注册
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			// 判断邮箱是否已注册
+			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 		}
 	}
 }
