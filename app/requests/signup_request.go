bcd3a76929119b9dafd9d6e3fe49673228fd7200
laugh <laugh@email.com>
2023-06-05 09:41:22 +0800

    refactor:重构验证器封装公共方法

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -11,26 +11,19 @@ type SignupPhoneExistRequest struct {
 	Phone string `json:"phone,omitempty" valid:"phone"`
 }
 
-func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
-	//自定义验证规则
+func SignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
+	// 自定义验证规则
 	rules := govalidator.MapData{
 		"phone": []string{"required", "digits:11"},
 	}
 	// 自定义验证出错时的提示
-	message := govalidator.MapData{
+	messages := govalidator.MapData{
 		"phone": []string{
 			"required:手机号为必填项，参数名称 phone",
 			"digits:手机号长度必须为 11 位的数字",
 		},
 	}
-	//配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		Messages:      message,
-		TagIdentifier: "valid", // 模型中的SignupPhoneExistRequest Struct 标签标识符
-	}
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
 
 // email验证器
@@ -38,13 +31,13 @@ type SignupEmailExistRequest struct {
 	Email string `json:"email,omitempty" valid:"email"`
 }
 
-func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
-	//自定义验证规则
+func SignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
+	// 自定义验证规则
 	rules := govalidator.MapData{
 		"email": []string{"required", "min:4", "max:30", "email"},
 	}
 	// 自定义验证出错时的提示
-	message := govalidator.MapData{
+	messages := govalidator.MapData{
 		"email": []string{
 			"required:Email 为必填项",
 			"min:Email 长度需大于 4",
@@ -52,12 +45,5 @@ func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]str
 			"email:Email 格式不正确，请提供有效的邮箱地址",
 		},
 	}
-	//配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		Messages:      message,
-		TagIdentifier: "valid", // 模型中的 SignupEmailExistRequest Struct 标签标识符
-	}
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
