f3821eaa9c314c09b290af12cea2fdce982c6b7f
laugh <laugh@email.com>
2023-06-06 14:44:24 +0800

    feat:创建响应各种格式的输出

---
+++ pkg/response/response.go
@@ -0,0 +1,112 @@
+package response
+
+import (
+	"GOHUB/pkg/logger"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+// JSON 响应 200 和 JSON 数据
+func JSON(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, data)
+}
+
+// Success 响应 200 和预设『操作成功！』的 JSON 数据
+func Success(c *gin.Context) {
+	JSON(c, gin.H{
+		"success": true,
+		"message": "操作成功！",
+	})
+}
+
+// Data 响应 200 和带 data 键的 JSON 数据
+func Data(c *gin.Context, data interface{}) {
+	JSON(c, gin.H{
+		"success": true,
+		"data":    data,
+	})
+}
+
+// Created 响应 201 和带 data 键的 JSON 数据
+func Created(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusCreated, gin.H{
+		"success": true,
+		"data":    data,
+	})
+}
+
+// CreatedJSON 响应 201 和 JSON 数据
+func CreatedJSON(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusCreated, data)
+}
+
+// Abort404 响应 404，未传参 msg 时使用默认消息
+func Abort404(c *gin.Context, msg ...string) {
+	c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+		"message": defaultMessage("数据不存在，请确定请求正确", msg...),
+	})
+}
+
+// Abort403 响应 403，未传参 msg 时使用默认消息
+func Abort403(c *gin.Context, msg ...string) {
+	c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+		"message": defaultMessage("权限不足，请确定您有对应的权限", msg...),
+	})
+}
+
+// Abort500 响应 500，未传参 msg 时使用默认消息
+func Abort500(c *gin.Context, msg ...string) {
+	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+		"message": defaultMessage("服务器内部错误，请稍后再试", msg...),
+	})
+}
+
+// BadRequest 响应 400，传参 err 对象，未传参 msg 时使用默认消息
+func BadRequest(c *gin.Context, err error, msg ...string) {
+	logger.LogIf(err)
+
+	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+		"message": defaultMessage("请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。", msg...),
+		"error":   err.Error(),
+	})
+}
+
+// Error 响应 404 或 422，未传参 msg 时使用默认消息
+func Error(c *gin.Context, err error, msg ...string) {
+	logger.LogIf(err)
+	// error 类型为『数据库未找到内容』
+	if err == gorm.ErrRecordNotFound {
+		Abort404(c)
+		return
+	}
+	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+		"message": defaultMessage("请求处理失败，请查看 error 的值", msg...),
+		"error":   err.Error(),
+	})
+}
+
+func ValidationError(c *gin.Context, errors map[string][]string) {
+	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+		"message": "请求验证不通过，具体请查看 errors",
+		"errors":  errors,
+	})
+}
+
+// Unauthorized 响应 401，未传参 msg 时使用默认消息
+// 登录失败、jwt 解析失败时调用
+func Unauthorized(c *gin.Context, msg ...string) {
+	c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+		"message": defaultMessage("请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。", msg...),
+	})
+}
+
+func defaultMessage(defaultMsg string, msg ...string) (message string) {
+	if len(msg) > 0 {
+		message = msg[0]
+	} else {
+		message = defaultMsg
+	}
+	return
+
+}
