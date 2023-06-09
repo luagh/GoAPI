3f23c976796cb3e727a9634ca9c5cee83b099593
laugh <laugh@email.com>
2023-06-09 15:04:27 +0800

    feat:通过邮箱找回密码

--- app/http/controllers/api/v1/auth/password_controller.go
+++ app/http/controllers/api/v1/auth/password_controller.go
@@ -34,3 +34,21 @@ func (pc *PasswordController) ResetByPhone(c *gin.Context) {
 		response.Success(c)
 	}
 }
+
+// ResetByEmail 使用 Email 和验证码重置密码
+func (pc *PasswordController) ResetByEmail(c *gin.Context) {
+
+	request := requests.ResetByEmailRequest{}
+	if ok := requests.Validate(c, &request, requests.ResetByEmail); !ok {
+		return
+	}
+
+	userModel := user.GetByEmail(request.Email)
+	if userModel.ID == 0 {
+		response.Abort404(c)
+	} else {
+		userModel.Password = request.Password
+		userModel.Save()
+		response.Success(c)
+	}
+}
