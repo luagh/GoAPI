2dcdf38e9e09ddafd5d41eb89102bb53d64a8b12
laugh <laugh@email.com>
2023-06-13 19:24:23 +0800

    feat:创建用户列表分页功能

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -1,6 +1,11 @@
 package user
 
-import "GOHUB/pkg/database"
+import (
+	"GOHUB/pkg/app"
+	"GOHUB/pkg/database"
+	"GOHUB/pkg/paginator"
+	"github.com/gin-gonic/gin"
+)
 
 // 判断email是否被注册
 func IsEmailExist(email string) bool {
@@ -47,5 +52,17 @@ func GetByEmail(email string) (userModel User) {
 // All 获取所有用户数据
 func All() (users []User) {
 	database.DB.Find(&users)
+	return
+}
+
+// Paginate 分页内容
+func Paginate(c *gin.Context, perPage int) (users []User, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(User{}),
+		&users,
+		app.V1URL(database.TableName(&User{})),
+		perPage,
+	)
 	return
 }
