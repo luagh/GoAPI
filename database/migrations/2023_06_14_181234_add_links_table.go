00f12d43c94e0039d230e6cb8c66694e2a24453c
laugh <laugh@email.com>
2023-06-14 18:14:50 +0800

    创建友情链接模型和迁移到数据库

---
+++ database/migrations/2023_06_14_181234_add_links_table.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"GOHUB/app/models"
+	"GOHUB/pkg/migrate"
+	"database/sql"
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Link struct {
+		models.BaseModel
+
+		Name string `gorm:"type:varchar(255);not null"`
+		URL  string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Link{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Link{})
+	}
+
+	migrate.Add("2023_06_14_181234_add_links_table", up, down)
+}
