47ae48ad6552f219d99c1e8749fa7fed189c8cde
laugh <laugh@email.com>
2023-06-14 18:04:23 +0800

    feat:创建话题列表功能

---
+++ database/seeders/topics_seeder.go
@@ -0,0 +1,28 @@
+package seeders
+
+import (
+    "fmt"
+    "GOHUB/database/factories"
+    "GOHUB/pkg/console"
+    "GOHUB/pkg/logger"
+    "GOHUB/pkg/seed"
+
+    "gorm.io/gorm"
+)
+
+func init() {
+
+    seed.Add("SeedTopicsTable", func(db *gorm.DB) {
+
+        topics  := factories.MakeTopics(10)
+
+        result := db.Table("topics").Create(&topics)
+
+        if err := result.Error; err != nil {
+            logger.LogIf(err)
+            return
+        }
+
+        console.Success(fmt.Sprintf("Table [%v] %v rows seeded", result.Statement.Table, result.RowsAffected))
+    })
+}
